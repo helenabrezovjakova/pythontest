3fbc4d9860cfaf5b6c812cbbd79af09724ee5a0e
helenabrezovjakova <helena.brezovjakova@gmail.com>
2017-07-17 19:45:52 +0100

    fixde print functions

--- manage.py
+++ manage.py
@@ -8,18 +8,18 @@ def welcome():
 
 @app.route("/signup", methods=["POST"])
 def sign_up():
-    print "hello"
+    print("hello")
     form_data = request.form
-    print "hello2"
+    print("hello2")
 
     email = form_data["email"]
     name = form_data["name"]
     surname = form_data["surname"]
     fav_color = form_data["fav_color"]
-    print email
-    print name
-    print surname
-    print fav_color
+    print(email)
+    print(name)
+    print(surname)
+    print(fav_color)
     return "All OK. Your name is {} {} and your email is {}. Your favorite color is {}!".format(name, surname, email, fav_color)
     #return "hello world {}".format(email)
 app.run(debug=True)
